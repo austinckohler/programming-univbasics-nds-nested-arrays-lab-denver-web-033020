a87e7b238d6746bca0f5bfe865d614ee7e22b2ac
Austin Kohler <austinckohler@gmail.com>
2020-03-25 16:07:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,7 +34,7 @@ def matrix_update(matrix, row, column, new_value)
  new = matrix[row][column] = new_value
  return matrix
 end
-
+puts matrix_update(sorted_matrix, 1, 1) = japan
 
 # def assembled_matrix
 # assembled_matrix = [
