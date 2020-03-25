2747737eae0acde58c1df5b1131d48f13de8da89
Austin Kohler <austinckohler@gmail.com>
2020-03-25 15:35:13 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -16,32 +16,32 @@ CONVENTIONAL_PRODUCE = [
   "Eggplant"
 ]
 
-def assembled_matrix
- assembled_matrix = [
- CONVENTIONAL_PRODUCE,
- ORGANIC_PRODUCE,
- ]
-end
+# def assembled_matrix
+# assembled_matrix = [
+# CONVENTIONAL_PRODUCE,
+# ORGANIC_PRODUCE,
+# ]
+# end
 
-def sorted_matrix
-  # Using Array literal syntax only, build another nested array that 
- # uses the arrays of conventional and organic produce as before.
- # However, this time, sort each internal array alphabetically by the first character
+# def sorted_matrix
+#   # Using Array literal syntax only, build another nested array that 
+# # uses the arrays of conventional and organic produce as before.
+# # However, this time, sort each internal array alphabetically by the first character
+# # sorted_matrix = [
+# #   ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], 
+# #   # ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
+# #   #   ]
 # sorted_matrix = [
- #   ["Grapefruit", "Pineapple", "Oranges", "Watermelon", "Eggplant"], 
-#   # ["Strawberries", "Potatoes", "Grapes", "Avocadoes", "Asparagus"]
-#   #   ]
-sorted_matrix = [
-CONVENTIONAL_PRODUCE.sort,
-ORGANIC_PRODUCE.sort]
-end
+# CONVENTIONAL_PRODUCE.sort,
+# ORGANIC_PRODUCE.sort]
+# end
 
-def matrix_lookup(matrix, row, column)
-return matrix[row][column]
+# def matrix_lookup(matrix, row, column)
+# return matrix[row][column]
 
-end
+# end
 
-def matrix_update(matrix, row, column, new_value)
-result = matrix[row][column] = new_value
-  return matrix
-end
+# def matrix_update(matrix, row, column, new_value)
+# result = matrix[row][column] = new_value
+#   return matrix
+# end
